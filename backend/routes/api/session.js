aea93fa306a64d03fcf1d46b55f8fa01ef5d93d7
Jamie Green <jjaggreen@gmail.com>
2024-07-17 22:56:42 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,20 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+        return res.json({
+            user: safeUser
+        });
+    } else return res.json({ user: null });
+});
+
 
 router.post('/', async (req, res, next) => {
     const { credential, password } = req.body;
@@ -43,7 +57,7 @@ router.post('/', async (req, res, next) => {
 
 router.delete('/', (_req, res) => {
     res.clearCookie('token');
-    return res.json({message: 'success'});
+    return res.json({ message: 'success' });
 });
 
 module.exports = router;
